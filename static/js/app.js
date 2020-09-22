66904378650928dacf5de599b144323f4208b4d6
餔餔 <52117569+willhwh@users.noreply.github.com>
2020-09-22 13:35:11 -0500

    function trigered successed

--- static/js/app.js
+++ static/js/app.js
@@ -8,5 +8,19 @@ var tableData = data;
 console.log(data);
 
 // YOUR CODE HERE!
-//tbody position
 var resultPresent = d3.select("tbody");
+
+var filterButton = d3.select('#filter-btn');
+var form = d3.select('#form')
+
+// Create event handlers 
+filterButton.on("click", filterTable);
+form.on("submit",filterTable);
+
+
+//function
+function filterTable(){
+    //prevent page from refreshing
+    d3.event.preventDefault();
+    console.log('filter function triggered.')
+};
