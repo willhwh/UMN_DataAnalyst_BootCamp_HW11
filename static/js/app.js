da3253ba5404017b92ddf0a9693ff703554555ba
餔餔 <52117569+willhwh@users.noreply.github.com>
2020-09-23 01:51:03 -0500

    select type function done

--- static/js/app.js
+++ static/js/app.js
@@ -15,7 +15,7 @@ var form = d3.select('#form');
 var searchType=d3.select('.selectpicker');
 var labelType=d3.select('#select-type');
 var inputPlace=d3.select('input');
-
+var enterLabel=d3.select('#enter-type')
 // Create event handlers 
 filterButton.on("click", filterTable); //use filter button to grigger function
 form.on("submit",filterTable); //use enter key to trigger function
@@ -26,33 +26,34 @@ var focusType = searchType.property('value');
 function focus(){
     focusType = searchType.property('value');
     
+
     if (focusType==='Date'){
         labelType.attr('for','date');
-        labelType.text('Enter a Date');
+        enterLabel.text('Enter a Date');
         inputPlace.attr('id','datetime');
         inputPlace.attr('placeholder','1/11/2011');
     }
     else if (focusType==='City'){
         labelType.attr('for','city')
-        labelType.text('Enter a City')
+        enterLabel.text('Enter a City')
         inputPlace.attr('id','city');
         inputPlace.attr('placeholder','la mesa');
     }
     else if (focusType==='State'){
         labelType.attr('for','state')
-        labelType.text('Enter a State')
+        enterLabel.text('Enter a State')
         inputPlace.attr('id','state');
         inputPlace.attr('placeholder','fl');
     }
     else if (focusType==='Country'){
         labelType.attr('for','country')
-        labelType.text('Enter a Country')
+        enterLabel.text('Enter a Country')
         inputPlace.attr('id','country');
         inputPlace.attr('placeholder','us');
     }
     else if (focusType==='Shape'){
         labelType.attr('for','shape')
-        labelType.text('Enter a Shape')
+        enterLabel.text('Enter a Shape')
         inputPlace.attr('id','shape');
         inputPlace.attr('placeholder','triangle');
     }
@@ -63,7 +64,7 @@ function focus(){
 function filterTable(){
     //prevent page from refreshing
     d3.event.preventDefault();
-
+    resultPresent.html("");
     console.log(`Using ${focusType} as filter`);
     console.log('filter function triggered.');
     console.log(`Search Term: ${inputValue}`);
